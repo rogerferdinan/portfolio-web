6b0a81470e8bac4c35f5b3e75c6fcc00b61b57f3
roger <rogerferdian@gmail.com>
2024-01-06 20:30:16 +0700

    minor fix

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,25 +1,46 @@
-const skillObserver = new IntersectionObserver((entries) => {
-    for(const entry of entries) {
-        if(entry.isIntersecting) {
-            entry.target.classList.add("skill-slide");
-        }
-    }
-})
-const experienceObserver = new IntersectionObserver((entries) => {
-    for(const entry of entries) {
-        if(entry.isIntersecting) {
-            entry.target.classList.add("experience-slide");
-        }
-    }
-})
+function createAnimationObserver(targetQuerySelector, addClassName) {
+    var items = document.querySelectorAll(targetQuerySelector);
+    const observer = new IntersectionObserver((entries) => {
+        entries.forEach(entry => {
+            if(entry.isIntersecting) {
+                entry.target.classList.add(addClassName);
+            }
+        })
+    })
+    items.forEach(item => {
+        observer.observe(item);
+    })
+    return observer;
+}
+
+// About Change color
+function createNavObserver(screenSelector, navSelector) {
+    const item = document.querySelector(screenSelector);
+    const observer = new IntersectionObserver((entries) => {
+        const nav_headers = document.getElementsByClassName("nav_item");
+        const nav_header = document.querySelector(navSelector);
+        entries.forEach(entry => {
+            if(entry.isIntersecting) {
+                for (nav of nav_headers) {
+                    nav.classList.remove("blue");
+                    nav.classList.add("black");
+                }
+                nav_header.classList.remove("black");
+                nav_header.classList.add("blue");
+            }
+        })
+    })
+    observer.observe(item);
+    return observer;
+}
 
 window.onload = () => {
-    var items = document.querySelectorAll(".skill-content");
-    items.forEach((item) => {
-        skillObserver.observe(item);
-    })
-    items = document.querySelectorAll(".experience-content");
-    items.forEach((item) => {
-        skillObserver.observe(item);
-    })
+    createAnimationObserver(".skill-content", "skill-slide");
+    createAnimationObserver(".experience-content", "experience-slide");
+
+    createNavObserver("#about", "#about_nav");
+    createNavObserver("#skill", "#skill_nav");
+    createNavObserver("#experiences", "#exp_nav");
+    createNavObserver("#projects", "#project_nav");
+    createNavObserver("#contact", "#contact_nav");
 }
